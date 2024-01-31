79b4f700d29fb3b147015ef56840efa605d642ee
hassan4702 <muhammadhassanshakil35066@gmail.com>
2024-01-31 19:29:57 +0500

    ads

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ import "./App.css";
 const App = () => {
   return (
     <main>
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9166564209735721"
+     crossorigin="anonymous"></script>
+      </head>
       <div className='main'>
         <div className='gradient' />
       </div>
